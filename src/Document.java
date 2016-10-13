a0323c79bda9e4556f35a50c5674ac5a6cf22128
frankgiak@gmail.com <frankgiak@gmail.com>
2016-10-13 15:46:16 +0200

    aggiunta qualcosa

---
+++ src/Document.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Francesco on 13/10/2016.
+ */
+public class Document {
+    private String comment;
+    private String number;
+    private Boolean[] sentiments;
+    private String FinaLabel;
+
+
+    //qui analizzo i vari boolean e applico l'algoritmo seguente :
+    //quando due array contengono la stessa annotazione allora aggiungo TRUE ad una certa label.
+    Boolean[] final_Sentiments(Boolean[] b1,Boolean[] b2,Boolean[] b3,Boolean[] b4){
+        Boolean[] b_final= new Boolean[6];
+
+        return b_final;
+    }
+}
--- src/main.java
+++ src/main.java
@@ -1,8 +1,48 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.regex.Pattern;
 /**
  * Created by Francesco on 13/10/2016.
  */
 public class main {
     public static void main(String[] args) {
-fadfadsfads
+        try {
+
+            HashMap<String,Boolean[]> features;
+            HashMap<String,String> comments;
+
+            BufferedReader bf= new BufferedReader(new FileReader(new File("src/Joint_dataset_group1.csv")));
+            String s=null;
+            Pattern pattern = Pattern.compile("[0-9]+");
+            int i=0;
+            int j=0;
+            while((s=bf.readLine())!=null) {
+                char[] cr = s.toCharArray();
+                j = 0;
+                //Cerco "NUMERO;;;;;;", il resto è il commento, ovviamente.
+                while (cr[j] != ';') {
+                    j++;
+                }
+                j = j + 7;
+                //Splitto sulla base del ; , il "Numero;;;;;;;"
+                String ssub = s.substring(0, j);
+                String[] ss = ssub.split(";");
+                //se il primo elemento della sottostringa è un numero (ovviamente..)
+                if (pattern.matcher(ss[0]).matches()) {
+                    Boolean[] bool = new Boolean[6];
+                    for (i = 0; i < ss.length-1; i++) {
+                        bool[i] = !ss[i + 1].equals("");
+                    }
+                }
+            }
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+
     }
+
+
 }
