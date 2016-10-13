128d4cf4bfbe311dd0151346aa664f2f1a876941
frankgiak@gmail.com <frankgiak@gmail.com>
2016-10-14 00:04:07 +0200

    Updated,buoanotte!! :)

--- src/Utils.java
+++ src/Utils.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Int;
+
 /**
  * Created by Francesco on 13/10/2016.
  */
@@ -11,7 +13,8 @@ public class Utils {
 
     List<Document> create_Docs_From_File(File file){
         List<Document> l=new ArrayList<>();
-        List<Boolean[]> annotations = new ArrayList<>();//NOTA QUESTO.
+        int[] arr = new int[6];
+
         String s = null;
         int num_Docs=4;
         BufferedReader bf = null;
@@ -23,19 +26,16 @@ public class Utils {
                 while ((s = bf.readLine()) != null) {
                     Document dc= new Document();
                     group[0] = s;
-                    set_Doc_detail(group[0],dc);
-                    get_sentiment(group[0]);
+                    update_Annotations(get_Values(group[0]),arr);
                     for (int m = 1; m < num_Docs; m++) {
-                        Boolean[] b= new Boolean[6];
                         if ((s = bf.readLine()) != null) {
                             group[m] = s;
-                            b= get_sentiment(group[m]);
-                            annotations.add(b);
+                            update_Annotations(get_Values(group[m]),arr);
                         }
                     }
-                    //per tutte le 4 annotazioni ottenute , chiama il metodo di dc passando le 4 annotazioni.
-                    //alla fine le aggiungi al DC.
-
+                    dc.setNumber(get_Id(group[0]));
+                    dc.setComment(get_Comment(group[0]));
+                    dc.setSentiments(results_From_Annotations(arr));
                     l.add(dc);
                 }
             } catch (IOException e) {
@@ -47,36 +47,55 @@ public class Utils {
         return l;
     }
 
-    private int  set_Doc_detail(String doc_Readed,Document d){
-        int i=0;
-        int j=0;
-        char[] cr = doc_Readed.toCharArray();
-        j = 0;
-        //Cerco "NUMERO;;;;;;", il resto è il commento, ovviamente.
-        while (cr[j] != ';') {
-            j++;
+    //questo recupera solo l'id.
+    private String get_Id(String doc_Readed) {
+        String[] ss = doc_Readed.split(";");
+        return ss[0];
+    }
+    //recupera i valori
+    private int[] get_Values(String doc_Readed) {
+        String[] ss = doc_Readed.split(";");
+        int a[] = new int[6];
+        for(int i=1;i<ss.length-1;i++){
+            //sullo splitting non ci siamo : problema -> se
+            if(ss[i].equals("x"))
+                    a[i-1]=1;
         }
-        j = j + 7;
-        //Splitto sulla base del ; , il "Numero;;;;;;;"
-        String ssub = doc_Readed.substring(0, j);
-        String[] ss = ssub.split(";");
-        d.setNumber(ss[0]);
-        //inserisco il commento , togliendo prima TUTTE le zozzate finali dei ;
-        int l=cr.length-1;
-        while (cr[l] == ';') {
-            l--;
-        }
-        d.setComment(doc_Readed.substring(j+1,l));
-        return j;//NON VA BENE IL J QUI, SERVE CHE POI NELL'ALTRO METODO TI VAI A FARE I BOOLEAN , QUINDI DEVI RESTITUIRE
+        return a;
     }
 
-
-    //se il primo elemento della sottostringa è un numero (ovviamente..)
-       /* if (pattern.matcher(ss[0]).matches()) {
-        Boolean[] bool = new Boolean[6];
-        for (i = 0; i < ss.length-1; i++) {
-            bool[i] = !ss[i + 1].equals("");
+    //recupera il commento
+    private String get_Comment(String doc_Readed) {
+        String[] ss = doc_Readed.split(";");
+        int i=1;
+        String comment = "";
+        while(i<ss.length-1){
+            if(!ss[i].equals("x") || !ss[i].equals("")){
+                comment+=ss[i];
+            }
+            i++;
         }
-    }*/
+      /*  char[] c= comment.toCharArray();
+        for(int m=c.length-1;m>=0;m--){
+            if
+        }*/
+      System.out.println(comment);
+        return comment;
+    }
 
+    private  void update_Annotations(int[] values,int[] arr){
+        for (int i = 0; i < values.length; i++) {
+            if (values[i]==1) {
+                arr[i] += 1;
+            }
+        }
+    }
+
+    private Boolean[] results_From_Annotations(int[] arr){
+        Boolean[] b= new Boolean[6];
+        for(int i=0;i<6;i++){
+            b[i] = arr[i] >= 2;
+        }
+        return b;
+    }
 }
